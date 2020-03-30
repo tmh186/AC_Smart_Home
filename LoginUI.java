a00fd0b679df1fb1f4d13e1a61045e86e06e70d1
Robert D Byers <rdbyers1@uab.edu>
2020-03-30 11:58:34 -0500

    Still just a WIP, this is for a login UI box that pops up, asks for a username and password, both of which are set as "admin". This could be for use in starting up the initial application or for accessing the administrative tab within the application.

---
+++ LoginUI.java
@@ -0,0 +1,65 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LoginUI implements ActionListener
+	{
+		int count = 0;
+		private static JLabel userlabel;
+		private static JLabel passlabel;
+		private static JFrame userframe;
+		private static JButton button;
+		private static JPanel userpanel;
+		
+		public static void main(String[] args)
+		{
+			userframe = new JFrame();
+			userpanel = new JPanel();
+			userframe.setSize(250, 250);
+			userframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			userframe.add(userpanel);
+			userpanel.setLayout(null);
+			userlabel = new JLabel("User: ");
+			userlabel.setBounds(100, 50, 85, 25);
+			userpanel.add(userlabel);
+			JTextField userText = new JTextField();
+			userText.setBounds(100, 20, 165, 25);
+			userpanel.add(userText);
+			passlabel = new JLabel("Password: ");
+			passlabel.setBounds(10, 50, 85, 25);
+			userpanel.add(passlabel);
+			JPasswordField passwordText = new JPasswordField();
+			passwordText.setBounds(100, 50, 165, 25);
+			userpanel.add(passwordText);
+			JButton button = new JButton("Login: ");
+			button.setBounds(10, 80, 80, 25);
+			userpanel.add(button);
+			JLabel success = new JLabel(" ");
+			success.setBounds(10, 110, 300, 25);
+			userpanel.add(success);
+			button.addActionListener(new LoginUI());
+			userframe.setVisible(true);
+			
+			
+			public actionPerformed (ActionEvent e)
+			{
+				String user = userText.getText();
+				String password = passwordText.getText();
+				System.out.println(user + ", " + password); 
+			}
+			if (userText.equals("admin") && passwordText.equals("admin"))
+			{
+				success.setText("Login Success");
+			}
+			else
+			{
+				success.setText("Login Failed");
+			}
+		}
+	}
