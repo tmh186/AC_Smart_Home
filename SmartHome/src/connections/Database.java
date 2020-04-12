abadd8897f29fdae68a01d87ea386a6de9b8e002
Samuel <Samuel@DESKTOP-BUUGO1D.attlocal.net>
2020-04-11 21:56:19 -0500

    Remove unnecessary weather and database objects

--- SmartHome/src/connections/Database.java
+++ SmartHome/src/connections/Database.java
@@ -1,6 +1,15 @@
 package connections;
 
+import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Database {
+	
+	private String DBURL  = "127.0.0.1";
+	private String DBPORT = "5432";
 
 	public Database() {
 		// TODO Auto-generated constructor stub
