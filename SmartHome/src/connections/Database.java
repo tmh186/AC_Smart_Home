6b3f077baa413d762c89d26ca4e65f9f6db9b24f
Samuel <Samuel@DESKTOP-BUUGO1D.attlocal.net>
2020-04-11 23:31:33 -0500

    Added initial testing for the database connection

--- SmartHome/src/connections/Database.java
+++ SmartHome/src/connections/Database.java
@@ -7,15 +7,34 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class Database {
-	
-	private String DBURL  = "127.0.0.1";
-	private String DBPORT = "5432";
+
+	private static String DBURL = "127.0.0.1";
+	private static String DBPORT = "5432";
+	private static String user = "team4";
+	private static String pass = "4team4";
+
+	static void testConnection() {
+		//taken from https://www.tutorialspoint.com/postgresql/postgresql_java.htm
+		//currently has a error from the database using the SSH Tunnel
+		String db = "jdbc:postgresql://"+DBURL+":"+DBPORT+"/"+user;
+		System.out.println(db);
+		Connection c = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection(db, user, pass);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		System.out.println("Opened database successfully");
+	}
 
 	void getAllDevices() {
-		//return all devices in the database	
+		// return all devices in the database
 	}
-	
+
 	void updateDeviceStatus(String DeviceName, String newStatus) {
-		//update a device status
+		// update a device status
 	}
 }
--- SmartHome/src/connections/DatabaseTest.java
+++ SmartHome/src/connections/DatabaseTest.java
@@ -4,7 +4,7 @@ public class DatabaseTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Database.testConnection();
 	}
 
 }
