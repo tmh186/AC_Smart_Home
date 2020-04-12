92f5b4afa075305d355102fb2a0f9459e4943115
Samuel <Samuel@DESKTOP-BUUGO1D.attlocal.net>
2020-04-12 17:32:34 -0500

    Improved database methods and connection options

--- SmartHome/src/connections/Database.java
+++ SmartHome/src/connections/Database.java
@@ -8,12 +8,10 @@ public class Database {
 	private static String DBPORT = "5432";
 	private static String user = "team4";
 	private static String pass = "4team4";
+	private static String db = "jdbc:postgresql://" + DBURL + ":" + DBPORT + "/" + user;
 
 	static void testConnection() throws SQLException {
-		//taken from https://www.tutorialspoint.com/postgresql/postgresql_java.htm
-		//currently has a error from the database using the SSH Tunnel
-		String db = "jdbc:postgresql://"+DBURL+":"+DBPORT+"/"+user;
-		//System.out.println(db);
+		// taken from https://www.tutorialspoint.com/postgresql/postgresql_java.htm
 		Connection c = null;
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -24,38 +22,74 @@ public class Database {
 			System.exit(0);
 		}
 		System.out.println("Opened database successfully");
-        c.close();
+		c.close();
 	}
-
-	static void getAllDevices() throws SQLException, ClassNotFoundException {
-		// return all devices in the database
+	
+	//Create the connection for the application
+	//close this connection when the program ends
+	static Connection initConnect(){
 		Connection c = null;
-		String db = "jdbc:postgresql://"+DBURL+":"+DBPORT+"/"+user;
-	      Statement stmt = null;
-	         Class.forName("org.postgresql.Driver");
-	         try {
-	        	 c = DriverManager.getConnection(db, user, pass);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-	         c.setAutoCommit(false);
-	         System.out.println("Opened database successfully");
-
-	         stmt = c.createStatement();
-	         ResultSet rs = stmt.executeQuery( "SELECT * FROM KITCHEN;" );
-	         while ( rs.next() ) {
-	             String id = rs.getString("devices");
-	             int name = rs.getInt("value");
-	             System.out.println("NAME = "+ id + ", VALUE = " + name);
-	         }
-	         rs.close();
-	         stmt.close();
-	         c.close();
+		try {
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.getConnection(db, user, pass);
+			c.setAutoCommit(false);
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.exit(0);
+		}
+		return c;
 	}
 
-	void updateDeviceStatus(String DeviceName, String newStatus) {
-		// update a device status
-		//may add a table specification
+	static void getAllDevices(Connection c) throws SQLException, ClassNotFoundException {
+		// return all devices in the database
+		//will need to be updated when the device table is added
+		Statement stmt = c.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT * FROM KITCHEN;");
+		while (rs.next()) {
+			String id = rs.getString("devices");
+			int name = rs.getInt("value");
+			System.out.println("NAME = " + id + ", VALUE = " + name);
+		}
+		rs.close();
+		stmt.close();
+	}
+
+	void updateDeviceStatus(String DeviceName, boolean newStatus) {
+		//update a device status
+		//Will not work until the database is updated
+	}
+	
+	static int getInternalTemp(Connection c) throws SQLException {
+		return getTemp(c, "'internal_temp'");
+	}
+	
+	static int getExternalTemp(Connection c) throws SQLException {
+		return getTemp(c, "'external_temp'");
+	}
+	
+	static int getTemp(Connection c, String name) throws SQLException {
+		Statement stmt = c.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT value FROM TEMPERATURE WHERE name="+name+";");
+		rs.next();
+		int value = rs.getInt("value");
+		stmt.close();
+		rs.close();
+		return value;
+	}
+	
+	static void updateExternalTemp(Connection c, int Temp) throws SQLException {
+		updateTemp(c, Temp, "'external_temp'");
+	}
+	
+	static void updateInternalTemp(Connection c, int Temp) throws SQLException {
+		updateTemp(c, Temp, "'internal_temp'");
+	}
+	
+	static void updateTemp(Connection c, int Temp, String name) throws SQLException {
+		Statement stmt = c.createStatement();
+		stmt.executeUpdate("UPDATE temperature SET value = "+Temp+ "WHERE name="+name+";");
+		c.commit();
+		stmt.close();
 	}
 }
--- SmartHome/src/connections/DatabaseTest.java
+++ SmartHome/src/connections/DatabaseTest.java
@@ -1,5 +1,6 @@
 package connections;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DatabaseTest {
@@ -8,7 +9,14 @@ public class DatabaseTest {
 		// TODO Auto-generated method stub
 		
 		//must be connected through the VPN for this to work or will error
-		Database.getAllDevices();
+		Connection c = Database.initConnect();
+		Database.getAllDevices(c);
+		System.out.println("Internal Temp: "+Database.getInternalTemp(c));
+		Database.updateInternalTemp(c, 90);
+		System.out.println("Internal Temp: "+Database.getInternalTemp(c));
+		Database.updateInternalTemp(c, 0);
+		System.out.println("External Temp: "+Database.getExternalTemp(c));
+		c.close();
 	}
 
 }
